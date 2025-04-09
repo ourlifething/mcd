0d80fb7170e2df4244738483d9e07a311023fac5
ourlifething <ryuji.hayama@gmail.com>
2025-04-09 23:06:07 +0900

    スペル修正

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/home.module.css";
 export default function Footer() {
   return (
-    <footer className={styles['footer']}>Portfolios -Ryuji Hayama- </footer>
+    <footer className={styles['footer']}>Portfolios -MCGDESIGN- </footer>
   )
 };
