88f3a5a0e0cb5be373cc8692be0c5335f36418d8
ourlifething <ryuji.hayama@gmail.com>
2025-04-09 19:21:37 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export', // 静的エクスポートを有効にする
+  images: {
+    unoptimized: true, // 画像最適化を無効にする（GitHub Pages で必要な場合）
+  },
+}
 
 module.exports = nextConfig
