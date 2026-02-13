e55716b9a9d077d88f81222e70c8352966284585
ourlifething <ryuji.hayama@gmail.com>
2026-02-14 00:35:14 +0900

    PR修正

--- src/app/components/profileDescription.tsx
+++ src/app/components/profileDescription.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/home.module.css'
 export function ProfileDescription () {
   return (
     <div className={styles['profile-text']}>
-      <p className={styles['header-text']}><b>ご覧いただきありがとうございます。</b>こちらは、葉山竜二のポートフォリオサイトです。<br/>制作物の一部を公開しております。<br/>
+      <p className={styles['header-text']}><b>ご覧いただきありがとうございます。</b>こちらは、葉山竜二のポートフォリオサイトです。<br/>制作物の一部を公開しております。Next.js,React,MongoDB<br/>
       </p>
     </div>
   )
